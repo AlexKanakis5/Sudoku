b908febcdef4c56fc2e2e31194149cef6164dd76
AlexKanakis5 <alexiskanakis@gmail.com>
2021-11-18 19:33:29 +0200

    Changed to 2D, col valid not work

--- Numbers.java
+++ Numbers.java
@@ -1,11 +1,17 @@
 /*
  H Numbers typwnei tous arithmous poy xreiazomaste
- give -> apla dinei enan tyxaio arithmo
- line -> dinei mia swsth grammh, periexei arithmous apo to 1-9 oi opoioi emfanizontai tyxaia 
-        alla mono mia fora se kathe grammh
 
+ Changed: H list pleon periexei oloklhro to table 
+            dhladh einai arraylist 2d 
 
- todo: na brw enan tropo na bazw kai diaforetikous arithmous se kathe sthlh
+            arxikopoieitai me 0 
+            meta elegxoume an yparxei o arithmos sth grammh opws prin 
+
+            ystera elegxoyme an yparxei aytos o arithmos sto sygkekgrimeno column 
+
+            kai an eimaste entaksei tote bazoume ton arithmo sto row 
+
+Not Working: kai pali, para tis prospatheies mou o pinakas exei duplicates sta columns
 */
 import java.util.ArrayList;
 import java.util.Random;
@@ -23,21 +29,68 @@ public class Numbers {
 
 
 
-    public static ArrayList<Integer> line()
+
+    public static ArrayList<ArrayList<Integer>> line()
     {
-        ArrayList<Integer> line = new ArrayList<Integer>();
+        ArrayList<ArrayList<Integer>> line = new ArrayList<>(9);
+        System.out.println("debug3");
 
-        for (int length = 0; length < 9; length++)
+
+        //dhmiourgeia line kai arxikopoihsh me mhdenika
+        //isws na to baleis se allh methodo
+        for(int i=0; i < 9; i++) {
+            line.add(new ArrayList());
+        }
+
+        for (int k = 0; k< 9; k++)
         {
-            int number_to_add = Numbers.give();
-
-            //oso o tyxaios arithmos yparxei sthn grammh zhtame allon apo thn give
-            while (line. contains(number_to_add))
+            for (int l =0; l<9; l++ )
             {
-                number_to_add = Numbers.give();
-            } 
-            //otan brethei arithmos poy den yparxei tote to bazoume sthn grammh
-            line.add(number_to_add);
+                line.get(k).add(0);
+            }
+        }
+
+        int redo = 1;
+
+        for (int row = 0; row < 9; row++)
+        {
+            for (int col=0; col< 9; col++)
+            {
+                int number_to_add = Numbers.give();
+                
+                //debug 
+                System.out.println(" row is " + row  + " col is " + col);
+
+                //oso o tyxaios arithmos yparxei sthn grammh zhtame allon apo thn give
+                //epishs an isxyei redo == 1 , to opoio energopoieitai otan yparxei to stoixeio
+                //sto column poy kanoume set tote bres allon tyxaio arithmo
+
+                //makari na doyleye etsi toulaxiston
+
+                //checkare an h sygkrish me ton telesth == einai swsth
+                while (line.get(row).contains(number_to_add) || (redo == 1))
+                {
+                    redo =  0;
+
+                    for(int j = 0; j < 9; j++)
+                    {
+                        if (line.get(j).get(col) == number_to_add)
+                        {
+                            redo = 1;
+                            System.out.println("reddit");
+                        }
+
+                    }
+                    number_to_add = Numbers.give();
+
+
+                } 
+                //otan brethei arithmos poy den yparxei tote to bazoume sthn grammh
+                line.get(row).set(col, number_to_add);
+
+            }
+            System.out.println(line.get(row));
+
         }
         return line;
     }
--- Table.java
+++ Table.java
@@ -1,6 +1,10 @@
 /*
     H Table typwnei to table
     periexei mono th methodo print pros to paron
+
+    Change: Aferesa ta symbola gia thn emfanish toy pinaka
+            me mperdeyan sto debuging 
+            kai sthn telikh den kserw kan an tha ta emfanizw etsi opws to ekana
 */
 
 import java.util.ArrayList;
@@ -9,53 +13,30 @@ public class Table{
     public static void print() {
         System.out.println("HelloChrysa!!");
 
-            // ftiaxnoyme ena arraylist line poy tha parei thn timh toy 
+            // ftiaxnoyme ena 2D arraylist line poy tha parei thn timh toy 
             // apo th methodo Numbers.line();
-            ArrayList<Integer> line = new ArrayList<Integer>() ;
+            ArrayList<ArrayList<Integer>> whole = new ArrayList<>(9);
 
-            int j = 0;
+            whole = Numbers.line();
 
-            /* 18 giti typonontai 18 grammes 
-             9 grammes me arithmous 
-            */ 
-            for (int i=0; i<18; i++)
+
+            int row = 0;
+            int col = 0;
+
+
+            //aplh ektypwsh, nothing to see here
+            for(row = 0; row<9; row++)
             {
-                line = Numbers.line();
-
-                // typonoume arithmes me mono arithmo 
-                if ( (i%2) != 0)
+                for(col = 0; col < 9; col ++)
                 {
-                    for(j=1; j<9; j++)
-                    {
-
-                        //typwnoume ena ena ta stoixeia tou line 
-                        // 1-1 giati thelw na bazw kai ta kena - payles 
-                        System.out.print(line.get(j));   
-                        if(j%3 == 0)
-                        {
-                            System.out.print(" | ");
-                        }
-                        else
-                        {
-                            System.out.print("  ");
-                        }
-                    }
-                    
-                    System.out.println(line.get(0));
+                    System.out.print(whole.get(row).get(col));
                 }
-                
-                //kathe 3 grammes typwnoyme payles " _ " anti gia arithmous
-                else if (i%3 == 0)
-                {
-                    for (int k=0; k<(j*3); k++)
-                    {
-                    System.out.print("_");
-                    }
-                    System.out.println();
-                    
-
-                }
+                System.out.println();
             }
+   
+
+                
+            
 
       }
     
